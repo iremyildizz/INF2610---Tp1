55ee92f1fcad3aa0fabdd7ab0d8e054093f9b140
Irem <iremyildiz02@gmail.com>
2024-09-21 14:33:45 -0400

    exo 2.1 done

--- Section1_SystemCalls/systemcalls_part1.c
+++ Section1_SystemCalls/systemcalls_part1.c
@@ -9,7 +9,38 @@
 */
 
 
+#include <stdio.h>
+#include <fcntl.h>
+
 int main () {
-    // TODO
+    char buffer;
+    int file;
+    __ssize_t bytesRead, bytesWritten;
+
+    file = open("systemcalls_output2.txt", O_WRONLY | O_TRUNC, 0644);
+    if(file == -1) {
+        perror("Appel système open a échoué");
+        return 1;
+    }
+    
+    write(1,"Saisissez votre texte suivi de CTRL-D :\n", 40);
+
+    while (bytesRead = read(0, &buffer, 1) > 0) {
+        bytesWritten = write(file, &buffer, bytesRead);
+        if(bytesWritten == -1){
+            perror("Appel système write a échoué");
+            close(file);
+            return 1;
+        }
+    }
+
+    if (bytesRead == -1) {
+        perror("Appel système read a échoué");
+        close(file);
+        return 1;
+    }
+
+    close(file);
     return 0;
 }
+
