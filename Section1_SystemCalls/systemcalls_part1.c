db4f432e72d967a8e85dc6dac1efec7ce4f0fcf3
Irem <iremyildiz02@gmail.com>
2024-09-21 13:08:37 -0400

    Ajout des fichiers

---
+++ Section1_SystemCalls/systemcalls_part1.c
@@ -0,0 +1,15 @@
+/*
+ * Ecole Polytechnique Montreal - GIGL
+ * Automne 2024
+ * SystemCalls - part1.c
+ *
+ * Ajoutez vos noms, prénoms et matricules
+ * Irem Yildiz 2125282
+ * Pablo Cabale Guerra 2129466
+*/
+
+
+int main () {
+    // TODO
+    return 0;
+}
