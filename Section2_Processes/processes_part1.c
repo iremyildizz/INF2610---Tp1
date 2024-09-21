0f85ed5d2c88a052cea17a597b4de6af2f73d5cf
Irem <iremyildiz02@gmail.com>
2024-09-21 17:56:35 -0400

    exo 3.1 done

--- Section2_Processes/processes_part1.c
+++ Section2_Processes/processes_part1.c
@@ -12,14 +12,30 @@ const int LEVEL_13_CHILDREN = 4;
 
 void question1()
 {
-    if(fork() == 0)
-        fork();
-    else if(fork() == 0)
-        fork();
+    if(fork() == 0){
+        registerProc(getpid(), getppid(), 1, 1);
+        if(fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 1);
+        }
+    }  
     else if(fork() == 0){
-        for(int i = 0; i < LEVEL_13_CHILDREN; i++)
-            if(fork() == 0)
+        registerProc(getpid(), getppid(), 1, 2);
+        if(fork() == 0) {
+            registerProc(getpid(), getppid(), 2, 2);
+        }
+    }  
+    else if(fork() == 0){
+        registerProc(getpid(), getppid(), 1, 3);
+        for(int i = 0; i < LEVEL_13_CHILDREN; i++){
+            if(fork() == 0){
+                registerProc(getpid(), getppid(), 2, i+LEVEL_13_CHILDREN-1);
                 break;
+            } 
+        }       
+    }
+    else{
+        wait(NULL);
+        registerProc(getpid(), getppid(), 0, 0);
     }
 }
 
